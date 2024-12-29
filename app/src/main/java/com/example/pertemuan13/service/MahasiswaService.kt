e2f114dfc908fdcdf32d859d8cb00bd6ff4d66cb
buyaramadhan <ytchochote@gmail.com>
2024-12-30 00:42:59 +0700

    menambah layanan MahasiswaService untuk mengelola data Mahasiswa

---
+++ app/src/main/java/com/example/pertemuan13/service/MahasiswaService.kt
@@ -0,0 +1,35 @@
+package com.example.pertemuan13.service
+
+import com.example.pertemuan13.model.Mahasiswa
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface MahasiswaService {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+
+    )
+
+    @POST ("insertmahasiswa.php")
+    suspend fun insertMahasiswa(@Body mahasiswa: Mahasiswa)
+
+    @GET("bacamahasiswa.php")
+    suspend fun  getAllMahasiswa(): List<Mahasiswa>
+
+    @GET("baca1mahasiswa.php/{nim}")
+    suspend fun getMahasiswabyNim(@Query("nim") nim:String):Mahasiswa
+
+    @PUT("editmahasiswa.php/{nim}")
+    suspend fun updateMahasiswa(@Query("nim")nim: String,@Body mahasiswa: Mahasiswa)
+
+    @DELETE ("deletemahasiswa.php/{nim}")
+    suspend fun  deleteMahasiswa(@Query("nim")nim : String) : retrofit2.Response<Void>
+
+}
