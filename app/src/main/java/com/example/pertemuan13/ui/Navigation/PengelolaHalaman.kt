3f93f99ec9c04ae053c64f00a9f6ab87254ff2f2
buyaramadhan <ytchochote@gmail.com>
2024-12-30 00:57:44 +0700

    menambah CostumeTopAppBar untuk navigasi yang bisa disesuaikan

---
+++ app/src/main/java/com/example/pertemuan13/ui/Navigation/PengelolaHalaman.kt
@@ -0,0 +1,43 @@
+package com.example.pertemuan13.ui.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan13.ui.view.DestinasiEntry
+import com.example.pertemuan13.ui.view.DestinasiHome
+import com.example.pertemuan13.ui.view.EntryMhsScreen
+import com.example.pertemuan13.ui.view.HomeScreen
+
+
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
