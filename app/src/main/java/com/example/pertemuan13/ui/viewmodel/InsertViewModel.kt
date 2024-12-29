bc45851d3fe00e98aa78f378654c88a556d5c746
buyaramadhan <ytchochote@gmail.com>
2024-12-30 00:50:08 +0700

    menambah InsertViewModel untuk mengelola penyisipan data Mahasiswa

---
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,66 @@
+package com.example.pertemuan13.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan13.Repository.MahasiswaRepository
+import com.example.pertemuan13.model.Mahasiswa
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val mhs:MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+       private set
+
+    fun updateInsertMhsState(insertUiEvent:InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent=insertUiEvent)
+    }
+
+    suspend fun  insertMhs(){
+        viewModelScope.launch {
+            try{
+                mhs.insretMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch(e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertUiState(
+    val insertUiEvent:InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+
+)
+
+fun InsertUiEvent.toMhs():Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nim,
+    alamat = nim,
+    jenisKelamin = nim,
+    kelas = nim,
+    angkatan = nim,
+
+)
+
+fun Mahasiswa.toUiStateMhs():InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
