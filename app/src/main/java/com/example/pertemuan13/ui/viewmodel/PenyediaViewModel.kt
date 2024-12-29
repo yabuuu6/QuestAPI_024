b35f34ac1ab8112ac2e3b6a357f91e0270bc3eea
buyaramadhan <ytchochote@gmail.com>
2024-12-30 05:22:45 +0700

    menambah inisialisasi DetailViewModel dan UpdateViewModel di PenyediaViewModel

--- app/src/main/java/com/example/pertemuan13/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.pertemuan13.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -11,7 +12,10 @@ object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiKontak().container.kontakRepository)}
         initializer { InsertViewModel(aplikasiKontak().container.kontakRepository)}
-    }
+        initializer { DetailViewModel(createSavedStateHandle(),aplikasiKontak().container.kontakRepository) }
+        initializer { UpdateViewModel(createSavedStateHandle(),aplikasiKontak().container.kontakRepository) }
+
+        }
 
     fun CreationExtras.aplikasiKontak():MahasiswaApplications=
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
